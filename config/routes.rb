b1de1e5c435f51ab6d3be7ecbb70a2c3ca0e7e66
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-29 16:36:29 +0200

    ADD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :challenges do
+    resources :reviews, only: %i[new create]
+    resources :bookings, only: %i[create]
+  end
+  resources :bookings, only: %i[destroy]
+  resources :reviews, only: %i[show edit destroy]
 end
