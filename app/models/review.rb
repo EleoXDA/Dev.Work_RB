7778cdeef18cbd5d42d9ea1f92efabfc5655ad1c
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-29 15:41:13 +0200

    REM: review model ADD: review column

--- app/models/review.rb
+++
@@ -1,4 +0,0 @@
-class Review < ApplicationRecord
-  belongs_to :challenge
-  belongs_to :user
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_124318) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_133938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_124318) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "review"
     t.index ["challenge_id"], name: "index_bookings_on_challenge_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
