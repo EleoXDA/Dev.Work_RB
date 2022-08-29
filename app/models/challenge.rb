94ef89e172cdbcb5bc524b2739f73b396f9ebba4
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-29 15:42:02 +0200

    FIX: undeleted lines

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,5 +2,4 @@ class Challenge < ApplicationRecord
   belongs_to :filter
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :reviews, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
-  has_many :reviews, dependent: :destroy
   has_many :challenges
 end
