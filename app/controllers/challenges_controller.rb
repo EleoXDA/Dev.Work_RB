ac13aabc2ee54bb6223c3b437045a4d98fb58672
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-29 17:39:32 +0200

    ADD: controllers

---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,33 @@
+class ChallengesController < ApplicationController
+  def show
+    @challenge = Challenge.find(params[:id])
+  end
+
+  def index
+    @challenges = Challenge.all
+  end
+
+  def new
+    @challenge = Challenge.new
+  end
+
+  def create
+    @challenge = Challenge.new(challenge_params)
+    if @challenge.save
+      redirect_to challenges_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @challenge = Challenge.find(params[:id])
+    @challenge.destroy
+  end
+
+  private
+
+  def challenge_params
+    params.require(:challenge).permit(:title, :content, :price_max, :deadline)
+  end
+end
---
+++ app/controllers/filters_controller.rb
@@ -0,0 +1,2 @@
+class FiltersController < ApplicationController
+end
