ab6bf2a490fb7c4534f74bd396eabaec20cc291d
Nirajan Wagle <nirajanwagle2@gmail.com>
2022-08-30 17:14:39 +0200

    Links and avatar working

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,18 @@
 
 # validates :title, :content, :price_max, :deadline, presence: true
 
-user = User.new(nickname: "Nriajaaoooo", name: "Nirajan", email: "stu1125615@as.com", password: "not my password")
-user.save!
-filter = Filter.new(name: "Java")
-challenge = Challenge.new(title: "center div", content: "Please center my div", price_max: 1000, deadline: Date.today, user:user)
-challenge.filter = filter
-challenge.save!
+# user = User.new(nickname: "Nriajaaoooo", name: "Nirajan", email: "stu1125615@as.com", password: "not my password")
+# user.save!
+# filter = Filter.new(name: "Java")
+# challenge = Challenge.new(title: "center div", content: "Please center my div", price_max: 1000, deadline: Date.today, user:user)
+# challenge.filter = filter
+# challenge.save!
+
+
+new_user = User.new(nickname: "Nriajaaoooo", name: "Weiss", email: "121@outlook.com", password: "not my password")
+new_user.save!
+
+filter = Filter.new(name: "Rails")
+new_challenge = Challenge.new(title: "Fix my code!!", content: "Please fix my code", price_max: 10100, deadline: Date.today, user: new_user)
+new_challenge.filter = filter
+new_challenge.save!
