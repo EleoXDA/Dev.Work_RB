a8d0fed2ac0eec3e3cb78ae23420e60bbe215b9b
Nirajan Wagle <nirajanwagle2@gmail.com>
2022-08-30 16:39:51 +0200

    NOT DONE with Avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# validates :title, :content, :price_max, :deadline, presence: true
+
+user = User.new(nickname: "Nriajaaoooo", name: "Nirajan", email: "stu1125615@as.com", password: "not my password")
+user.save!
+filter = Filter.new(name: "Java")
+challenge = Challenge.new(title: "center div", content: "Please center my div", price_max: 1000, deadline: Date.today, user:user)
+challenge.filter = filter
+challenge.save!
