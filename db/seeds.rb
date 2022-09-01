c5ae1f966d7513d9c773f9fb0ebe184e49adbca9
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-01 20:08:18 +0200

    Rearrange Seeds - FIX misspelling

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,39 @@ puts "Generating seeds..."
 lang = ['Java', 'Kotlin', 'Ruby', 'Javascript', 'Swift', 'Dart', 'CSS',
         'HTML', 'Bash', 'XML', 'C++', 'Rust', 'Pascal', 'Fortran', 'PHP',
         'Perl']
+
 title = ['Help needed', 'Please FIX ASAP', 'DEV to the rescue', 'Stuck in loading',
          "Doesn't Start up", 'Need a hand', 'Please please', 'Desperately in need',
          'New workplace needs a Dev', 'Fix this join us']
+
+addresses = ["Geneva, Switzerland",
+             "Zurich, Switzerland",
+             "Bern, Switzerland",
+             "University of Cambridge, Cambridge, United Kingdom",
+             "University of Oxford, Oxford, United Kingdom",
+             "Baneshwor, Kathmandu, Nepal",
+             "Berlin, Germany",
+             "Frankfurt, Germany",
+             "Paris, France",
+             "Bristol, Prais",
+             "Lyon, France",
+             "Kyiv, Ukraine",
+             "Madrid, Spain",
+             "Porto, Portugal",
+             "Lisbon, Portugal",
+             "Seville, Spain",
+             "Palermo, Italy",
+             "Vienna, Italy"]
+
+array = ["https://gravatar.com/avatar/38e6bc27f9d54cca67426924eb366a51?s=200&d=robohash&r=x",
+         "https://gravatar.com/avatar/649d7427af905218ac3e3fc2c2e6c2d4?s=200&d=robohash&r=x",
+         "https://gravatar.com/avatar/f00a238b97348910aeb3a18ac7920301?s=200&d=robohash&r=x",
+         "https://gravatar.com/avatar/07b5a65d00de168c8d8e4e1f8044b129?s=200&d=robohash&r=x",
+         "https://gravatar.com/avatar/01e150e9db07023122c524d0e58addea?s=200&d=robohash&r=x",
+         "https://gravatar.com/avatar/70f155619cdd9f45ea23f3039940ef0d?s=200&d=robohash&r=x",
+         "https://gravatar.com/avatar/e0399004b9c37b2c35525ad34c119620?s=200&d=robohash&r=x",
+         "https://gravatar.com/avatar/dc0d182da3d2c3ee00549396b264e60b?s=200&d=robohash&r=x",
+         "https://gravatar.com/avatar/18b00cafdca2767b9afb62b368110b7a?s=200&d=robohash&r=x"]
 
 filter_array = []
 20.times do
@@ -31,16 +61,6 @@ end
 
 user_array = []
 20.times do
-  array = [
-  "https://gravatar.com/avatar/38e6bc27f9d54cca67426924eb366a51?s=200&d=robohash&r=x",
-  "https://gravatar.com/avatar/649d7427af905218ac3e3fc2c2e6c2d4?s=200&d=robohash&r=x",
-  "https://gravatar.com/avatar/f00a238b97348910aeb3a18ac7920301?s=200&d=robohash&r=x",
-  "https://gravatar.com/avatar/07b5a65d00de168c8d8e4e1f8044b129?s=200&d=robohash&r=x",
-  "https://gravatar.com/avatar/01e150e9db07023122c524d0e58addea?s=200&d=robohash&r=x",
-  "https://gravatar.com/avatar/70f155619cdd9f45ea23f3039940ef0d?s=200&d=robohash&r=x",
-  "https://gravatar.com/avatar/e0399004b9c37b2c35525ad34c119620?s=200&d=robohash&r=x",
-  "https://gravatar.com/avatar/dc0d182da3d2c3ee00549396b264e60b?s=200&d=robohash&r=x",
-  "https://gravatar.com/avatar/18b00cafdca2767b9afb62b368110b7a?s=200&d=robohash&r=x"]
   user = User.new(nickname: Faker::FunnyName.name,
                   name: Faker::Name.name,
                   email: Faker::Internet.email,
@@ -51,33 +71,6 @@ user_array = []
 end
 
 50.times do
-  addresses = [
-    "Geneva, Switzerland",
-    "Zurich, Switzerland",
-    "Bern, Switzerland",
-    "University of Cambridge, Cambridge, United Kingdom",
-    "University of Oxford, Oxford, United Kingdom",
-    "Baneshwor, Kathmandu, Nepal",
-    "Berlin, Germany",
-    "Frankfurt, Germany",
-    "Paris, France",
-    "Bristol, Prais",
-    "Lyon, France",
-    "Kyiv, Ukraine",
-    "Madrid, Spain",
-    "Porto, Portugal",
-    "Lisbon, Portugal",
-    "Seville, Spain",
-    "Palermo, Italy",
-    "Vienna Italy"
-
-
-  ]
-
-  cities = [
-    "Geneva", "Cambridge", "Oxford", "Kathmandu", "Berlin", "Rome", "Florence", "Hamburg", "Bristol",
-    "Paris", "Frankfurt", "Lyon", "Kyiv", "Madrid", "Porto", "Lisbon", "Seville", "Palermo", "Vienna", "Zurich"
-  ]
   challenge = Challenge.new(title: title.sample,
                             content: Faker::Lorem.paragraphs(number: 2),
                             price_max: rand(100),
@@ -85,8 +78,7 @@ end
                             filter: filter_array.sample,
                             user: user_array.sample,
                             location: addresses.sample,
-                            cities: cities.sample
-                          )
+                            cities: cities.sample)
   challenge.save!
 end
 
