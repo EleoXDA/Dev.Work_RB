94d5057493a07467fe046acc91aa64dd86034db5
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-29 01:23:35 +0200

    Add GitHub auth to only allow signed up users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def after_sign_in_path_for(resource)
+  #   if current_user.location.nil?
+  #     edit_user_path(current_user)
+  #   else
+  #     users_path
+  #   end
+  # end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :photo])
+  end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end
