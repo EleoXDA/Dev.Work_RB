4ec72d1c036a155ee28a9101c1184b980ea34daf
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-29 01:26:17 +0200

    update user model - allow omniauth login w/ GitHub

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   after_create :set_default_avatar
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
   has_many :bookings, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :challenges
   has_one_attached :photo
 
@@ -16,5 +17,18 @@ class User < ApplicationRecord
     file = URI.open(path)
     photo.attach(io: file, filename: "userphoto.png", content_type: "image/png")
     save
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.nickname = auth.info.nickname
+      user.image = auth.info.image
+      user.html_url = auth.extra.raw_info.html_url
+      user.name = auth.info.name
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
 end
