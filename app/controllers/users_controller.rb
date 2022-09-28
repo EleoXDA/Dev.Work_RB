2d9d0ff87a84e503e49fdc248b794b7d92506c2f
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-29 01:25:08 +0200

    Allow user edit/update to sign in/up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(sign_up_params)
+    redirect_to users_path
+  end
 
   private
 
