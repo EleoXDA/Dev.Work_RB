c54e6d4f6143acbf4300064d798a82009048651f
Elbay Malikmammadov <eleo.666@gmail.com>
2022-09-29 22:39:58 +0200

    update map JS controller to simplify it

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,62 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
-
-// Connects to data-controller="map"
 export default class extends Controller {
   static values = {
     apiKey: String,
     markers: Array
   }
-  static targets = ["address"]
 
   connect() {
-    this.geocoder = new MapboxGeocoder({
-      accessToken: this.apiKeyValue,
-      types: "country,region,place,postcode,locality,neighborhood,address"
-    })
-    this.geocoder.addTo(this.element)
-    this.geocoder.on("result", event => this.#setInputValue(event))
-    this.geocoder.on("clear", () => this.#clearInputValue())
-
     mapboxgl.accessToken = this.apiKeyValue
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v11",
-    });
+      style: "mapbox://styles/mapbox/streets-v11"
+    })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
     this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl }))
   }
 
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach((marker) => {
-      bounds.extend([marker.lng, marker.lat])
-
-    })
-    this.map.fitBounds(bounds, {padding: 50, maxZoom: 50})
-  }
-
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window) // Add this
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(this.map)
-    });
-  }
-  #setInputValue(event) {
-    this.addressTarget.value = event.result["place_name"]
+    })
   }
 
-  #clearInputValue() {
-    this.addressTarget.value = ""
-  }
-
-  disconnect() {
-    this.geocoder.onRemove()
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 10, maxZoom: 12, duration: 0 })
   }
 }
