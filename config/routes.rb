e1255aaa1615a953159c552663c42c0b7bd776ce
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-01 01:30:56 +0200

    remove comments dependancy on bookings/offers

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :challenges, only: %i[show index new create destroy edit update] do
-    resources :bookings, only: %i[create update] do
-      resources :comments, only: %i[create]
-    end
+    resources :offers, only: %i[create update]
+    resources :comments, only: %i[create]
   end
   resources :users, only: [:index, :show, :edit, :update], path: "index" do
   end
-  resources :bookings, only: %i[destroy]
+  resources :offers, only: %i[destroy]
   # resources :comments, only: %i[edit update destroy]
   get '/my_challenges', action: :index, controller: 'challenges'
-  get '/my_bookings', action: :index, controller: 'bookings'
+  get '/my_offers', action: :index, controller: 'offers'
 end
