3cddfc10f0ff1164fcdc32119a21663c8c7aa07a
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-03 08:59:17 +0200

    ADD admin priveleges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_30_220602) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_065436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,6 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_220602) do
     t.string "uid"
     t.string "image"
     t.string "html_url"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
