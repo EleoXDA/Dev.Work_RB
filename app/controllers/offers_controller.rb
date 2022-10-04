dc02563237738959645255ef9147ac510ffa6c92
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-04 21:40:34 +0200

    add show offers function

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,6 +4,12 @@ class OffersController < ApplicationController
     @user_offers = Offer.where(user: current_user)
   end
 
+  def show
+    # @challenge = Challenge.find(params[:id])
+    @offer = Offer.where(user: current_user).where(challenge: @challenge)
+    authorize @offer
+  end
+
   def create
     @challenge = Challenge.find(params[:challenge_id])
     @offer = Offer.new(offer_params)
@@ -13,10 +19,9 @@ class OffersController < ApplicationController
     if @offer.save
       redirect_to challenge_path(@challenge), notice: "Offer has been made..."
     else
-      render 'challenges/show', status: :unprocessable_entity, locals: {'@offers': @challenge.offers}
+      render 'challenges/show', status: :unprocessable_entity, locals: { '@offers': @challenge.offers }
     end
   end
-
   # def comment
   #   @offer = offer.find(params[:id])
   #   @challenge = Challenge.find(params[:challenge_id])
